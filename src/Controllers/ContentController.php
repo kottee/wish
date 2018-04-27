bf75901443edaed9e1b0ac245515954a4c199505
kottee <kotteeswaranviit@gmail.com>
2018-04-27 12:35:14 +0530

    Add files via upload

---
+++ src/Controllers/ContentController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Wish\Controllers;
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class ContentController
+ * @package HelloWorld\Controllers
+ */
+class ContentController extends Controller
+{
+	/**
+	 * @param Twig $twig
+	 * @return string
+	 */
+	public function sayHello(Twig $twig):string
+	{
+		return $twig->render('Wish::content.hello');
+	}
+}
