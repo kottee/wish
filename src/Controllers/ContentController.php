e34c76e4f5899d0608d82a4c0cd29309ba3aea4f
kottee <kotteeswaranviit@gmail.com>
2018-04-27 16:04:53 +0530

    Update ContentController.php

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -3,6 +3,7 @@ namespace Wish\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
 
 /**
  * Class ContentController
@@ -14,8 +15,53 @@ class ContentController extends Controller
 	 * @param Twig $twig
 	 * @return string
 	 */
-	public function sayHello(Twig $twig):string
+	public function sayHelloT(Twig $twig):string
 	{
 		return $twig->render('Wish::content.hello');
 	}
+	
+	public function sayHello(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
+    	{
+		$itemColumns = [
+		    'itemDescription' => [
+			'name1',
+			'description'
+		    ],
+		    'variationBase' => [
+			'id'
+		    ],
+		    'variationRetailPrice' => [
+			'price'
+		    ],
+		    'variationImageList' => [
+			'path',
+			'cleanImageName'
+		    ]
+		];
+
+		$itemFilter = [
+		    'itemBase.isStoreSpecial' => [
+			'shopAction' => [3]
+		    ]
+		];
+
+		$itemParams = [
+		    'language' => 'en'
+		];
+
+		$resultItems = $itemRepository
+		    ->search($itemColumns, $itemFilter, $itemParams);
+
+		$items = array();
+		foreach ($resultItems as $item)
+		{
+		    $items[] = $item;
+		}
+		$templateData = array(
+		    'resultCount' => $resultItems->count(),
+		    'currentItems' => $items
+		);
+
+		return $twig->render('TopItems::content.TopItems', $templateData);
+    	}
 }
