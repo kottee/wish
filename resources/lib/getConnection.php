7ecf50983a2d2e43f45b054b1f87703f7ffd7eef
kottee <kotteeswaranviit@gmail.com>
2018-04-29 11:49:33 +0530

    Update getConnection.php

--- resources/lib/getConnection.php
+++ resources/lib/getConnection.php
@@ -1,11 +1,21 @@
 <?php
 
+use Wish\WishClient;
+use Wish\Model\WishTracker;
+use Wish\Exception\OrderAlreadyFulfilledException;
+use Wish\Model\WishReason;
+
+$token = 'b7a3bc5b0a0148208f2e80387f292366';
+$client = new WishClient($token,'sandbox');
+
+$changed_orders = $client->getAllChangedOrdersSince();
+
 $paymentResult = [];
 
 $paymentResult['version'] = SdkRestApi::getParam('pluginVersion', 'unknown');
 $paymentResult['url'] = SdkRestApi::getParam('urls');
 $paymentResult['id'] = SdkRestApi::getParam('id');
 $paymentResult['transactionId'] = '1234567890';
-$paymentResult['paymentUrl'] = 'paymentURI';
+$paymentResult['paymentUrl'] = 'TTT';
 
-return $paymentResult;
+return $changed_orders;
